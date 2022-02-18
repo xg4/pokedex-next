1516261462c06aa0b23220e3d6def148a6c8b388
xg4 <xingor4@gmail.com>
2022-02-18 17:28:43 +0800

    feat: prefetch pokemon detail info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { dehydrate, QueryClient, useQuery } from 'react-query';
 import Card from '../components/card';
-import { getPokemons } from '../services';
+import { getPokemonFromUrl, getPokemons } from '../services';
 
 export async function getStaticProps() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery('pokemons', () => getPokemons());
+  const result = queryClient.getQueryData<API.Pokemons>('pokemons');
 
+  if (result) {
+    await Promise.all(
+      result.results.map((pokemon) =>
+        queryClient.prefetchQuery(['pokemon', pokemon.url], () => getPokemonFromUrl(pokemon.url)),
+      ),
+    );
+  }
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
@@ -26,7 +33,7 @@ const Home: NextPage = () => {
           <h1 className="text-white text-3xl">Pok&#xE9;</h1>
         </div>
       </header>
-      <div className="container mx-auto flex flex-wrap p-6 justify-center">
+      <div className="container mx-auto flex flex-wrap p-6 justify-around">
         {data?.results.map((pokemon) => (
           <Card key={pokemon.url} pokemon={pokemon} />
         ))}

bddb8ed55b77348bc785a3d4ca0884b669209925
xg4 <xingor4@gmail.com>
2024-07-20 06:53:07 +0000

    chore: update deps

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Options} */
+const config = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
